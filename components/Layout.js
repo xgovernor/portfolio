8e94944a62c3e71f2477231e38f046ada0998e71
Abu Taher Muhammad <abut1081@gmail.com>
2022-01-15 14:31:37 -0800

    Design improvement in contact page

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,6 @@ import Head from 'next/head'
 import Navigation from './Navigation'
 import Footer from './Footer'
 import MousePointer from './MousePointer'
-// import AnimatedCursor from 'react-animated-cursor'
 
 
 export default function Layout({data, children}) {
@@ -14,15 +13,6 @@ export default function Layout({data, children}) {
                 <title>{data?.title}</title>
             </Head>
 
-            {/* <MousePointer /> */}
-            {/* <AnimatedCursor
-                innerSize={10}
-                outerSize={40}
-                color='0, 12, 26'
-                outerAlpha={0.2}
-                innerScale={0.7}
-                outerScale={1.5}
-            /> */}
             {/* Main Navigation */}
             <Navigation />
 
