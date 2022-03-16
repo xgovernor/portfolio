29b5940e1da19df79630d183bdadb9d4aa624d3a
Abu Taher Muhammad <abut1081@gmail.com>
2022-03-16 08:38:27 +0600

    Dev branch Creader & home page edited

--- pages/articles/[id].js
+++ pages/articles/[id].js
@@ -1,41 +1,41 @@
+/** @format */
+
 // Custom Components
-import SingleHeader from "../../components/article/SingleHeader";
-import SingleCover from "../../components/article/SingleCover";
+import { motion } from "framer-motion";
 import SingleContent from "../../components/article/SingleContent";
+import SingleCover from "../../components/article/SingleCover";
+import SingleHeader from "../../components/article/SingleHeader";
 import SingleUpdateStatus from "../../components/article/SingleUpdateStatus";
 import NavigationalArticles from "../../components/NavigationArticles";
-import { motion } from "framer-motion";
 import Layout from "../../views/shared/Layout/Layout";
 
-
 export default function ArticlesSingle() {
-    const singleArticleHeader = {
-        title: 'How Gorgias uses Vouchers to lessen webinar fatigue',
-        date: 'Published March 21, 2021',
-        cat: 'Front-end Development',
-    }
-    const singleArticleCover = {
-        url: "/images/singleCover.jpg",
-        alt: 'About banner Image',
-        width: 1088,
-        height: 370
-    }
-    
-    return (
-        <>
-            <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }} 
-            >
-                <Layout>
-                    <SingleHeader data={singleArticleHeader} />
-                    <SingleCover data={singleArticleCover} />
-                    <SingleContent />
-                    <SingleUpdateStatus />
-                    <NavigationalArticles />
-                </Layout>
-            </motion.div>
-        </>
-    )
+	const singleArticleHeader = {
+		title: "How Gorgias uses Vouchers to lessen webinar fatigue",
+		date: "Published March 21, 2021",
+		cat: "Front-end Development",
+	};
+	const singleArticleCover = {
+		url: "/images/singleCover.jpg",
+		alt: "About banner Image",
+		width: 1088,
+		height: 370,
+	};
+
+	return (
+		<>
+			<motion.div
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				exit={{ opacity: 0 }}>
+				<Layout>
+					<SingleHeader data={singleArticleHeader} />
+					<SingleCover data={singleArticleCover} />
+					<SingleContent />
+					<SingleUpdateStatus />
+					<NavigationalArticles />
+				</Layout>
+			</motion.div>
+		</>
+	);
 }
--- views/pages/about/AboutBody.js
+++ views/pages/about/AboutBody.js
@@ -1,161 +1,238 @@
-import Container from '../../shared/Container/Container';
+/** @format */
+
+import Container from "../../shared/Container/Container";
 
 const AboutBody = () => {
-    return (
-        <>
-            <section className="p_about_body_info">
-                
-                <Container>
-                    <div className='p_about_info_card'>
-                        {/* Left Side */}
-                        <div className='p_card__left'>
-                            <h4 className='p_heading_2'>Who am I ?</h4>
-                        </div>
+	return (
+		<>
+			<section className='p_about_body_info'>
+				<Container>
+					<div className='p_about_info_card'>
+						{/* Left Side */}
+						<div className='p_card__left'>
+							<h4 className='p_heading_2'>Who am I ?</h4>
+						</div>
 
-                        {/* Right Side */}
-                        <div className='p_card_right'>
-                            <p className='p_body'>There are some called Muhammad. Even only in Bangladesh. Before, my name was Abu Taher – and I only existed under this name once. But this is another story.</p>
-                            <p className='p_body'>What is true then as now: I am a designer and developer. To be precise, I solve people&apos;s problems thus and so that they get a smile on their face afterward look more beautiful than before or perform not like before. Maybe more practical to use. Websites, user interfaces, apps, and things like that. Some think I&apos;m totally creative and just one of those people; who make everything they put their hands on stunning. I see myself more as a problem solver who lives out his trained profession as a web developer with ambition and still wants to learn more today. I&apos;m more of a freethinker than a web developer. But that&apos;s just terminology anyway.</p>
-                        </div>
-                    </div>
+						{/* Right Side */}
+						<div className='p_card_right'>
+							<p className='p_body'>
+								There are some called Muhammad. Even only in
+								Bangladesh. Before, my name was Abu Taher – and
+								I only existed under this name once. But this is
+								another story.
+							</p>
+							<p className='p_body'>
+								What is true then as now: I am a designer and
+								developer. To be precise, I solve people&apos;s
+								problems thus and so that they get a smile on
+								their face afterward look more beautiful than
+								before or perform not like before. Maybe more
+								practical to use. Websites, user interfaces,
+								apps, and things like that. Some think I&apos;m
+								totally creative and just one of those people;
+								who make everything they put their hands on
+								stunning. I see myself more as a problem solver
+								who lives out his trained profession as a web
+								developer with ambition and still wants to learn
+								more today. I&apos;m more of a freethinker than
+								a web developer. But that&apos;s just
+								terminology anyway.
+							</p>
+						</div>
+					</div>
 
-                    <div className='p_about_info_card'>
-                        {/* Left Side */}
-                        <div className='p_card__left'>
-                            <h4 className='p_heading_2'>What I have learned</h4>
-                        </div>
+					<div className='p_about_info_card'>
+						{/* Left Side */}
+						<div className='p_card__left'>
+							<h4 className='p_heading_2'>What I have learned</h4>
+						</div>
 
-                        {/* Right Side */}
-                        <div className='p_card_right'>
-                            <p className='p_body'>After completing junior high school, I started learning web development while continuing my school. After gaining enough skillset began my apprenticeship in a small digital studio in the middle of the beautiful Sylhet in Bangladesh. I had great seniors like Saidul Islam(CEO of Freelancerlab) brother and Arif uncle, who trained my vision and taught me how to make things innovative and legible.</p>
-                            <p className='p_body'>UI/UX - HTML5 - CSS3 - Javascript - PHP - MySQL - Node.js - React.js - Next.js - MondoDB - GSAP - Strapi - GraphQL - Wordpress - etc.</p>    
-                        </div>
-                    </div>
+						{/* Right Side */}
+						<div className='p_card_right'>
+							<p className='p_body'>
+								After completing junior high school, I started
+								learning web development while continuing my
+								school. After gaining enough skillset began my
+								apprenticeship in a small digital studio in the
+								middle of the beautiful Sylhet in Bangladesh. I
+								had great seniors like Saidul Islam(CEO of
+								Freelancerlab) brother and Arif uncle, who
+								trained my vision and taught me how to make
+								things innovative and legible.
+							</p>
+							<p className='p_body'>
+								UI/UX - HTML5 - CSS3 - Javascript - PHP - MySQL
+								- Node.js - React.js - Next.js - MondoDB - GSAP
+								- Strapi - GraphQL - Wordpress - etc.
+							</p>
+						</div>
+					</div>
 
-                    <div className='p_about_info_card'>
-                        {/* Left Side */}
-                        <div className='p_card__left'>
-                            <h4 className='p_heading_2'>Experiences</h4>
-                        </div>
+					<div className='p_about_info_card'>
+						{/* Left Side */}
+						<div className='p_card__left'>
+							<h4 className='p_heading_2'>Experiences</h4>
+						</div>
 
-                        {/* Right Side */}
-                        <div className='p_card_right'>
-                            <p className='p_body'>I then worked with various people/agencies. During these years, in addition to great mentors and orders, a highlight was Muhammad Abdullah, for whom I was allowed to be a web developer myself. We still work together today on various freelance projects, but Abdullah has long been in the fast lane.</p>
-                        </div>
-                    </div>
+						{/* Right Side */}
+						<div className='p_card_right'>
+							<p className='p_body'>
+								I then worked with various people/agencies.
+								During these years, in addition to great mentors
+								and orders, a highlight was Muhammad Abdullah,
+								for whom I was allowed to be a web developer
+								myself. We still work together today on various
+								freelance projects, but Abdullah has long been
+								in the fast lane.
+							</p>
+						</div>
+					</div>
 
-                    <div className='p_about_info_card'>
-                        {/* Left Side */}
-                        <div className='p_card__left'>
-                            <h4 className='p_heading_2'>What else do I do?</h4>
-                        </div>
+					<div className='p_about_info_card'>
+						{/* Left Side */}
+						<div className='p_card__left'>
+							<h4 className='p_heading_2'>What else do I do?</h4>
+						</div>
 
-                        {/* Right Side */}
-                        <div className='p_card_right'>
-                            <p className='p_body'>In addition to workload, I enjoy attending webinars and meetings on web design and development or helping out my buddies by solving their problems on the Discord server.</p>
-                        </div>
-                    </div>
-                    
-                    <div className='p_about_info_card'>
-                        {/* Left Side */}
-                        <div className='p_card__left'>
-                            <h4 className='p_heading_2'>Work</h4>
-                        </div>
+						{/* Right Side */}
+						<div className='p_card_right'>
+							<p className='p_body'>
+								In addition to workload, I enjoy attending
+								webinars and meetings on web design and
+								development or helping out my buddies by solving
+								their problems on the Discord server.
+							</p>
+						</div>
+					</div>
 
-                        {/* Right Side */}
-                        <div className='p_card_right'>
-                            <table className='p_table'>
-                                <tbody>
-                                    <tr>
-                                        <td className="__left">
-                                            <p className="p_body">Junior Web Developer</p>
-                                        </td>
-                                        <td className="__center">
-                                            <p className="p_body">Freelancerlabbd</p>
-                                        </td>
-                                        <td className="__right">
-                                            <p className="p_body">2020 - 2021</p>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td className="__left">
-                                            <p className="p_body">Front-end Designer & Developer</p>
-                                        </td>
-                                        <td className="__center">
-                                            <p className="p_body">DigiYotta</p>
-                                        </td>
-                                        <td className="__right">
-                                            <p className="p_body">2019 - 2020</p>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td className="__left">
-                                            <p className="p_body">Freelance Developer</p>
-                                        </td>
-                                        <td className="__center">
-                                            <p className="p_body">...</p>
-                                        </td>
-                                        <td className="__right">
-                                            <p className="p_body">2018 - Current</p>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                    
-                    <div className='p_about_info_card'>
-                        {/* Left Side */}
-                        <div className='p_card__left'>
-                            <h4 className='p_heading_2'>Education & Certification</h4>
-                        </div>
+					<div className='p_about_info_card'>
+						{/* Left Side */}
+						<div className='p_card__left'>
+							<h4 className='p_heading_2'>Work</h4>
+						</div>
 
-                        {/* Right Side */}
-                        <div className='p_card_right'>
-                            <table className='p_table'>
-                                <tbody>
-                                    <tr>
-                                        <td className="__left">
-                                            <p className="p_body">Complete Web Development</p>
-                                        </td>
-                                        <td className="__center">
-                                            <p className="p_body">Programming Hero</p>
-                                        </td>
-                                        <td className="__right">
-                                            <p className="p_body">2021</p>
-                                        </td>
-                                    </tr>
+						{/* Right Side */}
+						<div className='p_card_right'>
+							<table className='p_table'>
+								<tbody>
+									<tr>
+										<td className='__left'>
+											<p className='p_body'>
+												Junior Web Developer
+											</p>
+										</td>
+										<td className='__center'>
+											<p className='p_body'>
+												Freelancerlabbd
+											</p>
+										</td>
+										<td className='__right'>
+											<p className='p_body'>
+												2020 - 2021
+											</p>
+										</td>
+									</tr>
+									<tr>
+										<td className='__left'>
+											<p className='p_body'>
+												Front-end Designer & Developer
+											</p>
+										</td>
+										<td className='__center'>
+											<p className='p_body'>DigiYotta</p>
+										</td>
+										<td className='__right'>
+											<p className='p_body'>
+												2019 - 2020
+											</p>
+										</td>
+									</tr>
+									<tr>
+										<td className='__left'>
+											<p className='p_body'>
+												Freelance Developer
+											</p>
+										</td>
+										<td className='__center'>
+											<p className='p_body'>...</p>
+										</td>
+										<td className='__right'>
+											<p className='p_body'>
+												2018 - Current
+											</p>
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
 
-                                    <tr>
-                                        <td className="__left">
-                                            <p className="p_body">Computer Science & Engineering</p>
-                                        </td>
-                                        <td className="__center">
-                                            <p className="p_body">Leadign University</p>
-                                        </td>
-                                        <td className="__right">
-                                            <p className="p_body">2021 - Current</p>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td className="__left">
-                                            <p className="p_body">IELTS</p>
-                                        </td>
-                                        <td className="__center">
-                                            <p className="p_body">British Council</p>
-                                        </td>
-                                        <td className="__right">
-                                            <p className="p_body">2022</p>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </Container>
-            </section>
-        </>
-    )
-}
+					<div className='p_about_info_card'>
+						{/* Left Side */}
+						<div className='p_card__left'>
+							<h4 className='p_heading_2'>
+								Education & Certification
+							</h4>
+						</div>
 
-export default AboutBody
+						{/* Right Side */}
+						<div className='p_card_right'>
+							<table className='p_table'>
+								<tbody>
+									<tr>
+										<td className='__left'>
+											<p className='p_body'>
+												Complete Web Development
+											</p>
+										</td>
+										<td className='__center'>
+											<p className='p_body'>
+												Programming Hero
+											</p>
+										</td>
+										<td className='__right'>
+											<p className='p_body'>2021</p>
+										</td>
+									</tr>
+
+									<tr>
+										<td className='__left'>
+											<p className='p_body'>
+												Computer Science & Engineering
+											</p>
+										</td>
+										<td className='__center'>
+											<p className='p_body'>
+												Leadign University
+											</p>
+										</td>
+										<td className='__right'>
+											<p className='p_body'>
+												2021 - Current
+											</p>
+										</td>
+									</tr>
+									<tr>
+										<td className='__left'>
+											<p className='p_body'>IELTS</p>
+										</td>
+										<td className='__center'>
+											<p className='p_body'>
+												British Council
+											</p>
+										</td>
+										<td className='__right'>
+											<p className='p_body'>2022</p>
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</Container>
+			</section>
+		</>
+	);
+};
+
+export default AboutBody;
