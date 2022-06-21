06d70599f0eb0d43b2d325802499137232a73a33
Abu Taher Muhammad <abut1081@gmail.com>
2022-06-22 02:55:39 +0600

    Parallax effect adde to page banner

--- components/PageBanner.js
+++ components/PageBanner.js
@@ -1,14 +1,23 @@
 // Core Components
-import Image from 'next/image'
+import Image from "next/image";
+import { Parallax } from "react-scroll-parallax";
 
-
-export default function PageBanner({dataImg}) {
-    // console.log(dataImg)
-    return (
-        <>
-            <section className="p_page_banner">
-	            <Image src={dataImg.url} width={dataImg.width} height={dataImg.height} alt={dataImg.alt} layout="responsive" />
-            </section>
-        </>
-    )
+export default function PageBanner({ dataImg }) {
+	// console.log(dataImg)
+	return (
+		<>
+			<section className="p_page_banner">
+				<Parallax speed={-20}>
+					<Image
+						src={dataImg.url}
+						width={dataImg.width}
+						height={dataImg.height}
+						alt={dataImg.alt}
+						priority={true}
+						layout="responsive"
+					/>
+				</Parallax>
+			</section>
+		</>
+	);
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,47 +1,45 @@
 /** @format */
 
 import { AnimatePresence } from "framer-motion";
+import Head from "next/head";
 import Script from "next/script";
+import { ParallaxProvider } from "react-scroll-parallax";
 import "../styles/index.sass";
-import Head from 'next/head';
 
-const GOOGLE_GTAG_MEASUREMENT_ID = 'GTM-N3QF76C';
+const GOOGLE_GTAG_MEASUREMENT_ID = "GTM-N3QF76C";
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
-		<Head>
-		{/* Google Tag Manager */}
-			<Script
-				id="GTag"
-				strategy="afterInteractive"
-				dangerouslySetInnerHTML={{
-					__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+			<Head>
+				{/* Google Tag Manager */}
+				<Script
+					id="GTag"
+					strategy="afterInteractive"
+					dangerouslySetInnerHTML={{
+						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 						new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 						j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 						'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 						})(window,document,'script','dataLayer','${GOOGLE_GTAG_MEASUREMENT_ID}');`,
-				}}
-			/>
-			{/* End Google Tag Manager */}
-</Head>
+					}}
+				/>
+				{/* End Google Tag Manager */}
+			</Head>
 			{/* Google Tag Manager (noscript) */}
 			<noscript>
 				<iframe
 					src={`https://www.googletagmanager.com/ns.html?id=${GOOGLE_GTAG_MEASUREMENT_ID}`}
 					height="0"
 					width="0"
-					style={{ display: "none", visibility: "hidden" }}
-				></iframe>
+					style={{ display: "none", visibility: "hidden" }}></iframe>
 			</noscript>
 			{/* End Google Tag Manager (noscript) */}
 
 			<AnimatePresence exitBeforeEnter>
-				{/* <SmoothScrollProvider options={{ smooth: true, lerp: 0.1 }}> */}
-				{/* <div data-scroll-container > */}
-				<Component {...pageProps} />
-				{/*</div> */}
-				{/* </SmoothScrollProvider> */}
+				<ParallaxProvider>
+					<Component {...pageProps} />
+				</ParallaxProvider>
 			</AnimatePresence>
 		</>
 	);
