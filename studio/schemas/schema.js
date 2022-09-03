e80525d207c0fb6e9018bf405feccb5db421586f
Abu Taher Muhammad <abut1081@gmail.com>
2022-09-03 12:50:05 +0600

    Studio added

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -6,10 +6,18 @@ import account from "./account";
 import blockContent from "./blockContent";
 import category from "./category";
 import post from "./post";
+import project from "./project";
 import topic from "./topic";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
 	name: "default",
-	types: schemaTypes.concat([post, account, category, topic, blockContent]),
+	types: schemaTypes.concat([
+		post,
+		project,
+		category,
+		topic,
+		account,
+		blockContent,
+	]),
 });
