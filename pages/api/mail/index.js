14337bd6b80bb3efd08386f9b67d1d3733144537
Abu Taher Muhammad <abut1081@gmail.com>
2022-09-04 17:10:29 +0600

    log updated

--- pages/api/mail/index.js
+++ pages/api/mail/index.js
@@ -1,6 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default async function handler(req, res) {
+	console.log("RESPONSE: ", {
+		status: req.status,
+		method: req.method,
+		body: req.body,
+		query: req.query,
+		headers: req.headers,
+	});
+
 	switch (req.method) {
 		case "GET":
 			return res.status(200).json({
