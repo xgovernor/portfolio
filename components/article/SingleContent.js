b85cc70262d5b0985305e8a93c99d245b93f30b7
abutahermuhammad <abut1081@gmail.com>
2022-11-23 21:56:04 +0600

    Single article updated

--- components/article/SingleContent.js
+++ components/article/SingleContent.js
@@ -1,49 +1,158 @@
 // Core Components
-import Image from "next/image"
-
+import Image from "next/image";
 
 export default function SingleContent() {
-    return (
-        <>
-            <section className="p_single_content">
-                <div className="p_container_single">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <h2>Section 1.10.32 of &quot;de Finibus Bonorum et Malorum&quot;, written by Cicero in 45 BC</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  return (
+    <>
+      <section className="p_single_content">
+        <div className="p_container_single">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <h2>
+            Section 1.10.32 of &quot;de Finibus Bonorum et Malorum&quot;,
+            written by Cicero in 45 BC
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
 
-                    <blockquote>Hi! I&apos;m Julius Guevarra. I have a strong passion for design and technology. I specialize in Front End Development and UI/UX Design and my passion is all about building elegant and professional user interfaces and websites. I also do branding and identity design such as logo design, letterhead and business card, along with photo editing, image cropping and other graphic design services.</blockquote>
-                    
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <blockquote>
+            Hi! I&apos;m Julius Guevarra. I have a strong passion for design and
+            technology. I specialize in Front End Development and UI/UX Design
+            and my passion is all about building elegant and professional user
+            interfaces and websites. I also do branding and identity design such
+            as logo design, letterhead and business card, along with photo
+            editing, image cropping and other graphic design services.
+          </blockquote>
 
-                    <div className="p_2c_img">
-                        <div className="p_2c_left">
-                            <Image src={`/images/img1.jpg`} width={413} height={341.77} alt="sdf asdf" layout="responsive" />
-                        </div>
-                        <div className="p_2c_right">
-                            <Image src={`/images/img2.jpg`} width={413} height={341.77} alt="sdf asdf" layout="responsive" />
-                        </div>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <pre>
-                        <code>
-                            &lt;!DOCTYPE&gt;<br/>
-                            &lt;html&gt;<br/>
-                                &lt;title&gt;Page title&lt;/title&gt;<br/>
-                            &lt;/html&gt;<br/>
-                        </code>
-                    </pre>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
 
-                    <div className="p_1c_img">
-                        <Image src={`/images/img3.jpg`} width={862} height={344.3} alt="sdf asdf" layout="responsive" />
-                    </div>
-                    
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <div className="p_2c_img">
+            <div className="p_2c_left">
+              <Image
+                src={`/images/img1.jpg`}
+                width={413}
+                height={341.77}
+                alt="sdf asdf"
+                layout="responsive"
+              />
+            </div>
+            <div className="p_2c_right">
+              <Image
+                src={`/images/img2.jpg`}
+                width={413}
+                height={341.77}
+                alt="sdf asdf"
+                layout="responsive"
+              />
+            </div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <pre>
+            <code>
+              &lt;!DOCTYPE&gt;
+              <br />
+              &lt;html&gt;
+              <br />
+              {"    "}&lt;title&gt;Page title&lt;/title&gt;
+              {"    "}
+              <br />
+              {"    "}
+              <br />
+              {"    "}&lt;script&gt;
+              {"    "}
+              <br />
+              {"        "}function hello() {`{}`}
+              <br />
+              <br />
+              {"        "}const hi = () =&gt; hello()
+              {"    "}
+              <br />
+              {"    "}&lt;/script&gt;
+              <br />
+              <br />
+              &lt;/html&gt;
+              <br />
+            </code>
+          </pre>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
 
-                </div>
-            </section>
-        </>
-    )
+          <div className="p_1c_img">
+            <Image
+              src={`/images/img3.jpg`}
+              width={862}
+              height={344.3}
+              alt="sdf asdf"
+              layout="responsive"
+            />
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </section>
+    </>
+  );
 }
--- components/article/SingleCover.js
+++ components/article/SingleCover.js
@@ -2,19 +2,19 @@ import Image from "next/image";
 import Container from "../components/Container";
 
 export default function SingleCover({ data }) {
-	return (
-		<>
-			<section className="p_single_cover">
-				<Container>
-					<Image
-						src={data?.url}
-						width={data?.width}
-						height={data?.height}
-						alt={data?.alt}
-						layout="responsive"
-					/>
-				</Container>
-			</section>
-		</>
-	);
+  return (
+    <>
+      <section className="p_single_cover">
+        <Container>
+          <Image
+            src={data?.url}
+            width={data?.width}
+            height={data?.height}
+            alt={data?.alt}
+            layout="responsive"
+          />
+        </Container>
+      </section>
+    </>
+  );
 }
