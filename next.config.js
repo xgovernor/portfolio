a5afe4a8c4104270e4fe89288cf3383058bb705c
abutahermuhammad <abut1081@gmail.com>
2022-11-24 19:47:42 +0600

    headers removed

--- next.config.js
+++ next.config.js
@@ -3,39 +3,6 @@ module.exports = {
     domains: ["cdn.sanity.io", "images.pexels.com"],
   },
   reactStrictMode: false,
-  async headers() {
-    return [
-      {
-        source: "/*",
-        headers: [
-          {
-            key: "Strict-Transport-Security",
-            value: "max-age=63072000; includeSubDomains; preload", // Matched parameters can be used in the value
-          },
-          {
-            key: "X-DNS-Prefetch-Control",
-            value: "on",
-          },
-          {
-            key: "X-XSS-Protection",
-            value: "1; mode=block",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "SAMEORIGIN",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "cache-control",
-            value: "public, max-age=63072000, must-revalidate",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 const STUDIO_REWRITE = {
