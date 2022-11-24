f29adf7ac9780482c5a9dbffd8c50363a52adb67
Abu Taher Muhamamd <49128672+abutahermuhammad@users.noreply.github.com>
2022-11-24 15:52:59 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -12,6 +12,21 @@ module.exports = {
             key: 'Strict-Transport-Security',
             value: 'max-age=63072000; includeSubDomains; preload', // Matched parameters can be used in the value
           },
+          {
+  key: 'X-DNS-Prefetch-Control',
+  value: 'on'
+},
+          {
+  key: 'X-XSS-Protection',
+  value: '1; mode=block'
+},
+          {
+  key: 'X-Frame-Options',
+  value: 'SAMEORIGIN'
+},{
+  key: 'X-Content-Type-Options',
+  value: 'nosniff'
+},
         ],
       },
     ]
