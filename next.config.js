42913a8177ec3ff1c7f83559d2f8635bc90ad331
Abu Taher Muhammad <abut1081@gmail.com>
2023-05-01 13:50:24 +0600

    FIX: code optimized

--- next.config.js
+++ next.config.js
@@ -1,21 +1,23 @@
+const STUDIO_REWRITE = {
+  source: "/studio/:path*",
+  destination: process.env.NODE_ENV === "development" ? "http://localhost:3333/studio/:path*" : "/studio/index.html",
+};
+
 module.exports = {
   images: {
     domains: ["cdn.sanity.io", "images.pexels.com"],
   },
+  rewrites: () => [STUDIO_REWRITE],
   reactStrictMode: false,
   poweredByHeader: false,
-  experimental: {
-    optimizeCss: true,
-    scrollRestoration: true,
-  },
   async headers() {
     return [
       {
-        source: "/*",
+        source: '/(.*)',
         headers: [
           {
             key: "Strict-Transport-Security",
-            value: "max-age=63072000; includeSubDomains; preload", // Matched parameters can be used in the value
+            value: "max-age=63072000; includeSubDomains; preload",
           },
           {
             key: "X-DNS-Prefetch-Control",
@@ -41,16 +43,4 @@ module.exports = {
       },
     ];
   },
-};
-
-const STUDIO_REWRITE = {
-  source: "/studio/:path*",
-  destination:
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3333/studio/:path*"
-      : "/studio/index.html",
-};
-
-module.exports = {
-  rewrites: () => [STUDIO_REWRITE],
 };
