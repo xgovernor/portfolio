8adc89b0d38b151c7ce905ee8fa4a7eea96c2ea9
Abu Taher Muhammad <abut1081@gmail.com>
2023-05-01 13:53:10 +0600

    Optimization: Headers updated

--- next.config.js
+++ next.config.js
@@ -16,28 +16,28 @@ module.exports = {
         source: '/(.*)',
         headers: [
           {
-            key: "Strict-Transport-Security",
-            value: "max-age=63072000; includeSubDomains; preload",
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload',
           },
           {
-            key: "X-DNS-Prefetch-Control",
-            value: "on",
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
           },
           {
-            key: "X-XSS-Protection",
-            value: "1; mode=block",
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
           },
           {
-            key: "X-Frame-Options",
-            value: "SAMEORIGIN",
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
           },
           {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
           },
           {
-            key: "cache-control",
-            value: "public, max-age=63072000, must-revalidate",
+            key: 'Referrer-Policy',
+            value: 'same-origin',
           },
         ],
       },
