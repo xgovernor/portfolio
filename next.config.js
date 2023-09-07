4da669f0de45c1976dec556ffaab88dc9d11f205
Abu Taher Muhammad <abut1081@gmail.com>
2023-09-08 01:55:21 +0600

    Added `CSP`, `permission-policy`, `Prefetch`, etc

--- next.config.js
+++ next.config.js
@@ -1,46 +1,66 @@
-const STUDIO_REWRITE = {
-  source: "/studio/:path*",
-  destination: process.env.NODE_ENV === "development" ? "http://localhost:3333/studio/:path*" : "/studio/index.html",
-};
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'random-nonce-value';
+  child-src at-mah.vercel.app;
+  style-src 'self' at-mah.vercel.app;
+  font-src 'self';
+`;
 
 module.exports = {
   images: {
     domains: ["cdn.sanity.io", "images.pexels.com"],
   },
-  rewrites: () => [STUDIO_REWRITE],
   reactStrictMode: false,
   poweredByHeader: false,
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=31536000; includeSubDomains; preload',
-          },
-          {
-            key: 'X-DNS-Prefetch-Control',
-            value: 'on',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
-          },
-          {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block',
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'same-origin',
-          },
-        ],
-      },
-    ];
-  },
+  headers: async () => ([
+    {
+      source: '/(.*)',
+      headers: [
+        {
+          key: 'X-Developed-By',
+          value: 'Dot9',
+        },
+        {
+          key: 'X-Developed-By-Url',
+          value: 'https://at-mah.vercel.app',
+        },
+        {
+          key: 'X-DNS-Prefetch-Control',
+          value: 'on',
+        },
+        {
+          key: 'Strict-Transport-Security',
+          value: 'max-age=31536000; includeSubDomains; preload',
+        },
+        {
+          key: 'X-DNS-Prefetch-Control',
+          value: 'on',
+        },
+        {
+          key: 'X-Frame-Options',
+          value: 'SAMEORIGIN',
+        },
+        {
+          key: 'X-XSS-Protection',
+          value: '1; mode=block',
+        },
+        {
+          key: 'X-Content-Type-Options',
+          value: 'nosniff',
+        },
+        {
+          key: 'Referrer-Policy',
+          value: 'same-origin',
+        },
+        {
+          key: 'Permissions-Policy',
+          value: 'geolocation=(), browsing-topics=()',
+        },
+        {
+          key: 'Content-Security-Policy',
+          value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+        },
+      ],
+    },
+  ]),
 };
