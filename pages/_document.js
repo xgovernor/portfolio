3d00c776b91aa9da3febdbaae7e2891273e73459
Abu Taher Muhammad <abut1081@gmail.com>
2023-09-08 01:12:08 +0600

    `crossOrigin` improved

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ export default function Document() {
 
         {/* Google Custom Font */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap"
           rel="stylesheet"
