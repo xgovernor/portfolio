dab106fcd682386a46a963fa6b9924fdb5dd52b4
Abu Taher Muhammad <abut1081@gmail.com>
2024-12-08 14:39:31 +0600

    feat: added case study page

---
+++ src/components/BlockContent/index.jsx
@@ -0,0 +1,94 @@
+import Block from '@sanity/block-content-to-react'
+import { imageBuilder } from '../../utils/sanity';
+import Image from 'next/image';
+
+const CodeBlock = (props) => {
+  return (
+    <pre data-language={props.node.language}>
+      <code>{props.node.code}</code>
+    </pre>
+  )
+}
+
+const TableBlock = (props) => {
+  return (
+    <div className="p_table">
+      <table>
+        {(props.node.rows).map((row, i) => (
+          (row._type === "tableRow") && (
+            <tr key={row?._key}>
+              {(row.cells).map((cel, i) => (
+                <td key={i + row._key}>{cel}</td>
+              ))}
+            </tr>
+          )
+        ))}
+      </table>
+    </div>
+  )
+}
+
+const imageBlock = (props) => {
+  const src = imageBuilder(props.node.asset).url();
+  return (
+        <figure className="relative w-full h-auto mb-2 last:mb-0">
+          <Image
+            className='object-contain w-full h-auto'
+            src={src}
+            width={1920}
+            height={1080}
+            alt={props.node.caption}
+          />
+          {props?.node?.caption && (
+            <caption>{props.node.caption}</caption>
+          )}
+        </figure>
+  )
+}
+
+const projectContentBlock = (props) => {
+  return (
+    <div className="px-5 md:px-10 py-10 md:py-16 flex flex-col">
+      {props.node.subheading && <h6 className='text-[13px] mb-2'>{props.node.subheading}</h6>}
+      {props.node.heading && <h2 className='max-w-5xl text-5xl'>{props.node.heading}</h2>}
+
+      {props.node.paragraphs && props.node.paragraphs.length > 0 && <div className="relative w-full mt-7 flex max-md:flex-col justify-end gap-5 max-md:flex-wrap">
+        {props.node.paragraphs && props.node.paragraphs.map((paragraph, i) => (
+          <p key={i} className='w-full md:w-[calc((100%-60px)/4)] font-sans !text-[14px] !leading-[160%]'>{paragraph}</p>
+        ))}
+      </div>}
+  </div>
+  )
+}
+
+const dualImageBlock = (props) => {
+  return (
+    <div className="grid grid-cols-2 gap-2">
+      <Image src={imageBuilder(props.node.image1).url()} width={910} height={1080} alt="" />
+      <Image src={imageBuilder(props.node.image2).url()} width={910} height={1080} alt="" />
+    </div>
+  )
+}
+
+const serializers = {
+  types: {
+    code: CodeBlock,
+    sizeChart: TableBlock,
+    image: imageBlock,
+    projectContent: projectContentBlock,
+    dualImage: dualImageBlock
+  },
+}
+const BlockContent = ({ content }) => {
+  return (
+    <Block
+        className="font-sans"
+        blocks={content}
+        projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
+        dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
+        serializers={serializers}
+    />
+  )
+}
+
+export default BlockContent
--- src/pages/work/[id]/index.jsx
+++ src/pages/work/[id]/index.jsx
@@ -4,23 +4,59 @@ import ProjectHeader from '../../../components/ProjectHeader'
 import ProjectInfo from '../../../components/ProjectInfo'
 import COVER from './../../../assets/images/case.webp'
 import ProjectContent from '../../../components/ProjectContent'
+import { groq } from 'next-sanity'
+import { getClient, imageBuilder } from '../../../utils/sanity'
 
-const data = {
-  title: "O.C.O Technology: Building with C02",
-  // cover: COVER,
-  role: "UI/UX, Frontend, Backend",
-  technology: "NextJS, NodeJS, Sanity.io, etc",
-  intro: "Simulating the multiple lighting possibilities of a new two-tone gradient effect light fixture using WebGL technology. Prior to launching Flame, an outdoor light fixture used in architectural lighting projects, Hydrel’s division of Acuity Brands aimed to elevate its sales experience to the next level. The product’s dual light beam, which creates a unique flame-like appearance, inspired its name"
-}
+// *[_type == "project" && slug.current == $slug] | order(_updatedAt desc)[0] {
+const PROJECT_QUERY = groq`*[_type == "project" && slug.current == $slug] | order(_updatedAt desc)[0]
+{
+  _id,
+  thumbnail,
+  projectName,
+  title,
+  liveUrl,
+  excerpt,
+  body,
+  role,
+  technology,
+}`;
 
-const Project = () => {
+const Project = ({ project }) => {
+  console.log(project)
   return (
     <Layout>
-      <ProjectHeader title={data.title} cover={COVER.src} />
-      <ProjectInfo role={data.role} tech={data.technology} intro={data.intro} />
-      <ProjectContent />
+      <ProjectHeader title={project.title} cover={imageBuilder(project.thumbnail).width(1920).height(1080).url()} />
+      <ProjectInfo role={Array(project.role || []).join(', ')} tech={Array(project.technology || []).join(', ')} intro={project.excerpt} />
+      <ProjectContent content={project.body} />
     </Layout>
   )
 }
+
+export async function getServerSideProps({ params }) {
+  const { id: slug } = params;
+
+  try {
+    const project = await getClient(false).fetch(PROJECT_QUERY, { slug });
+
+    if (!project) {
+      return {
+        notFound: true,
+      };
+    }
+
+    return {
+      props: {
+        project,
+        slug,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching article data:", error);
+    return {
+      notFound: true,
+    };
+  }
+}
+
 
 export default Project
