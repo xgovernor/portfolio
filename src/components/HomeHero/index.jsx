8ff6bcd44b5cea88039c2830aef9452859c0123a
xgovernor <abut1081@gmail.com>
2025-03-01 07:08:42 +0600

    refactor: simplify HomeHero title rendering by removing array mapping

--- src/components/HomeHero/index.jsx
+++ src/components/HomeHero/index.jsx
@@ -3,16 +3,14 @@ import Container from "../Container";
 import { SOCIAL_MEDIA } from "../../assets/data/links";
 
 function HomeHero({
-  title = ["Designer, developer", "and freethinker."],
   cvURL = "https://drive.google.com/file/d/1WZctwHtbSrtyLavPO4epSTORAwCDeF8k/view?usp=sharing"
 }) {
     return (
       <section className="">
         <Container className="w-full max-w-screen-xl pt-[55px] pb-10 md:pt-[85px] md:pb-[100px] lg:pt-[105px] lg:pb-[80px] xl:pt-[130px] xl:pb-[70px]">
           <h1 className="font-black flex flex-col items-center text-center text-black uppercase text-[32px] leading-tight lg:leading-[86px] md:text-6xl lg:text-7xl">
-            {title.map((line, index) => (
-              <span key={index}>{line}</span>
-            ))}
+              <span>Designer, developer</span>
+              <span>and freethinker.</span>
           </h1>
 
           <div className="mt-2.5 flex gap-4 justify-center">
