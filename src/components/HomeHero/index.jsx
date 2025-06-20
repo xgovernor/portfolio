2543e6fbecf635a3187344720d5c8f2c47b6df09
xgovernor <abut1081@gmail.com>
2025-06-21 05:13:47 +0600

    refactor: improve structure and styling in HomeHero and PageHeader components

--- src/components/HomeHero/index.jsx
+++ src/components/HomeHero/index.jsx
@@ -8,30 +8,30 @@ function HomeHero({
     return (
       <section>
         <Container className="w-full max-w-screen-xl pt-[55px] pb-10 md:pt-[85px] md:pb-[100px] lg:pt-[105px] lg:pb-[80px] xl:pt-[130px] xl:pb-[65px]">
-          <h1 className="font-black flex flex-col items-center text-center text-black uppercase text-[32px] leading-tight lg:leading-[86px] md:text-6xl lg:text-7xl">
-              <span>Designer, developer</span>
-              <span>and freethinker.</span>
+          <h1 className="flex flex-col items-center text-center text-[32px] leading-tight font-black text-black uppercase md:text-6xl lg:text-[80px] lg:leading-[92px]">
+            <span>Designer, developer</span>
+            <span>and freethinker.</span>
           </h1>
 
-          <div className="mt-2.5 flex gap-4 justify-center">
-              {SOCIAL_MEDIA.map((social, index) => (
-                <a
-                  key={index}
-                  href={social.url}
-                  title={social.name}
-                  area-label={`${social.name} profile`}
-                  className="text-[#B2B5B7] hover:text-black"
-                  target="_blank"
-                >
-                  {social.icon}
-                </a>
-              ))}
+          <div className="mt-2.5 flex justify-center gap-4">
+            {SOCIAL_MEDIA.map((social, index) => (
+              <a
+                key={index}
+                href={social.url}
+                title={social.name}
+                area-label={`${social.name} profile`}
+                className="text-[#B2B5B7] hover:text-black"
+                target="_blank"
+              >
+                {social.icon}
+              </a>
+            ))}
           </div>
 
-          <div className="flex justify-center items-center max-xl:mt-20 xl:mt-[120px]">
+          <div className="flex items-center justify-center max-xl:mt-20 xl:mt-[120px]">
             <Link
               href={cvURL}
-              className="font-medium font-serif rounded-full text-sm px-5 py-2.5 me-2 mb-2 border-[#B2B5B7] border text-[#000c19] hover:text-[#313131]"
+              className="me-2 mb-2 rounded-full border border-[#B2B5B7] px-5 py-2.5 font-serif text-sm font-medium text-[#000c19] hover:text-[#313131]"
               download
               target="_blank"
             >
--- src/components/PageHeader/index.jsx
+++ src/components/PageHeader/index.jsx
@@ -4,18 +4,16 @@ import Container from "../Container";
 function PageHeader({heading, subheading }) {
   return (
     <section>
-       <Container className="max-md:min-h-[60vh] max-md:h-auto max-md:flex max-md:flex-col max-md:justify-end max-md:px-[30px] max-md:pt-24 max-md:pb-[40px] md:max-xl:pt-[100px] md:max-lg:pb-[80px] md:max-lg:px-[92px] lg:pb-[100px]  lg:max-xl:px-[150px] xl:max-w-[972px] xl:pt-[120px]">
-          <h4 className="uppercase text-[#000c19] opacity-[0.7]  font-bold text-xs md:mb-2 lg:mb-2.5 xl:text-sm">
-            {heading}
-          </h4>
+      <Container className="max-md:flex max-md:h-auto max-md:min-h-[60vh] max-md:flex-col max-md:justify-end max-md:px-[30px] max-md:pt-24 max-md:pb-[40px] md:max-xl:pt-[100px] md:max-lg:px-[92px] md:max-lg:pb-[80px] lg:pb-[100px] lg:max-xl:px-[150px] xl:max-w-[972px] xl:pt-[120px]">
+        <h4 className="text-xs font-bold text-[#000c19] uppercase opacity-[0.7] md:mb-2 lg:mb-2.5 xl:text-sm">
+          {heading}
+        </h4>
 
-          <h1 className=" font-black -tracking-[0.0015em] [word-spacing:4px] text-[#000c19] text-left flex flex-col items-start uppercase text-[34px] leading-10 md:text-[42px] md:leading-[48px] lg:max-xl:w-[724px] lg:text-[52px] lg:leading-[62px] xl:text-[68px] xl:leading-[64px]">
-            {subheading?.map((line, i) => (
-                <span key={i}>
-                  {line}
-                </span>
-              ))}
-          </h1>
+        <h1 className="flex flex-col items-start text-left text-[34px] leading-10 font-black -tracking-[0.0015em] text-[#000c19] uppercase [word-spacing:4px] md:text-[42px] md:leading-[48px] lg:text-[52px] lg:leading-[62px] lg:max-xl:w-[724px] xl:text-[68px] xl:leading-[68px]">
+          {subheading?.map((line, i) => (
+            <span key={i}>{line}</span>
+          ))}
+        </h1>
       </Container>
     </section>
   );
